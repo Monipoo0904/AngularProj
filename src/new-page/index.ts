745e2d19bf8268dc0d8ec0705feff4d9e64b93ee
Monique Reed <monique@scratchwerk.tech>
2022-11-09 21:10:55 -0500

    initial commit

---
+++ src/new-page/index.ts
@@ -0,0 +1,2 @@
+export * from "./newpage.component";
+
---
+++ src/new-page/newpage.component.ts
@@ -0,0 +1,4 @@
+import { Component } from "@angular/core";
+
+@Component({templateUrl: 'newpage.component.html'})
+export class NewpageComponent{}
